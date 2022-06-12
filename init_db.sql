50cc874d87c804d2f5e4a41941713aab21716d52
Muhammad Syafiq <muhammadsyafiq@MacBook-Air-Muhammad.local>
2022-06-12 16:32:50 +0800

    Currently computing the similarity

--- init_db.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `publications_tab`;
-
-CREATE TABLE `publications_tab` (
-  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-  `author` varchar(512) NOT NULL,
-  `vector` blob NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
