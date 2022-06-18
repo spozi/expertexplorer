a47c9102147e416721b35a4344e7fac362cdc543
Muhammad Syafiq <muhammadsyafiq@MacBook-Air-Muhammad.local>
2022-06-18 20:36:21 +0800

    Photo is finish TODO:Example articles and UI tweaks

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from filecmp import DEFAULT_IGNORES
+# from filecmp import DEFAULT_IGNORES
 from flask import Flask, render_template, request, flash, send_file
-from flask_mysqldb import MySQL
-from flask_paginate import Pagination
+# from flask_mysqldb import MySQL
+# from flask_paginate import Pagination
 from main import match_job
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import seaborn as sns
-import io
-from mysql_manager import MySQLManager
+# import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+# import seaborn as sns
+# import io
+# from mysql_manager import MySQLManager
 from scibert import SciBERT
 import pandas as pd
 import numpy as np
 from scipy import spatial
-import json
-from itertools import chain
+# import json
+# from itertools import chain
 from functools import reduce
+from collections import OrderedDict
+
 
 
 USER = "syafiq"
@@ -40,128 +42,6 @@ app.config['MYSQL_PORT'] = PORT
 # app.config['SECRET_KEY'] = 'something only you know'
 
 scibert = SciBERT()
-
-
-# def prepare_result(query):
-#     mysql_m = MySQLManager()
-#     df_combined = match_job(query, scibert)
-#     meanimage_list = []
-#     varimage_list = []
-#     for index in df_combined.index:
-#         meanimage_list.append('image_base64("img_bytes"): ' + str(index))
-#         varimage_list.append('image_base64("img_bytes"): ' + str(index))
-#     global result
-#     result = save_result(df_combined.copy(), meanimage_list, varimage_list)
-#     mysql_m.delete_candidate()
-#     for i in result.index:
-#         record = {}
-#         record["photo"] = result["Photo"][i]
-#         record["author"] = result["Candidate"][i]
-#         record["university"] = result["university"][i]
-#         record["doc_count"] = str(result["Docs"][i])
-#         record["avg_sim"] = str(result["AVG_SIM"][i])
-#         record["mean"] = str(result["Average_Similarity"][i])
-#         record["meanimage"] = result["Image(Avg)"][i]
-#         record["var"] = str(result["Variance"][i])
-#         record["varimage"] = result["Image(Var)"][i]
-#         record["related_count"] = str(result["Related"][i])
-#         mysql_m.insert_candidate(record)
-#     print("Candidate data inserted", flush=True)
-#     return True
-
-
-# def save_result(df, mean_image_list, var_image_list):
-#     mean_list = []
-#     var_list = []
-#     docs_list = []
-#     avg_mean_list = []
-#     related_list = []
-#     for i in df.index:
-#         mean_tuple = (df["mean_2018"][i], df["mean_2019"][i], df["mean_2020"][i], df["mean_2021"][i])
-#         avg_mean = sum(mean_tuple) / len(mean_tuple)
-#         avg_mean_list.append(avg_mean) 
-#         mean_list.append(mean_tuple) 
-#         var_list.append((df["var_2018"][i], df["var_2019"][i], df["var_2020"][i], df["var_2021"][i]))
-#         docs_list.append(sum((df["docs_2018"][i], df["docs_2019"][i], df["docs_2020"][i], df["docs_2021"][i])))
-#         related_list.append(sum((df["r_2018"][i], df["r_2020"][i], df["r_2021"][i])))
-#     df.drop(["mean_2018", "mean_2019", "mean_2020", "mean_2021"], axis=1, inplace=True)
-#     df.drop(["var_2018", "var_2019", "var_2020", "var_2021"], axis=1, inplace=True)
-#     df.drop(["docs_2018", "docs_2019", "docs_2020", "docs_2021"], axis=1, inplace=True)
-#     df.drop(["r_2018", "r_2020", "r_2021"], axis=1, inplace=True)
-#     df.rename(columns={'photo': 'Photo'}, inplace=True)
-#     df.rename(columns={'author': 'Candidate'}, inplace=True)
-#     df["Average_Similarity"] = mean_list
-#     df["Image(Avg)"] = mean_image_list
-#     df["Variance"] = var_list
-#     df["Image(Var)"] = var_image_list
-#     df["Docs"] = docs_list
-#     df["Related"] = related_list 
-#     df["AVG_SIM"] = avg_mean_list
-#     return df
-
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# #search job and company
-# @app.route('/expert', methods=['POST', 'GET'])
-# def show1():
-#     entries = 1000
-#     limit = 1000
-#     ts = 0.7
-#     conn = mysql.connection
-#     cursor = conn.cursor()
-    
-#     if request.method == "POST":
-#         query = request.form['query']
-#         if query != "":
-#             isMatched = prepare_result(query)
-#         if isMatched:
-#             conn = mysql.connection
-#             cursor = conn.cursor()
-#             cursor.execute("SELECT COUNT(*) FROM candidate WHERE  Avg_sim > 0.2")
-#             conn.commit()
-#             total = cursor.fetchall()
-#             total_count = total[0][0]
-#             message = ""
-#             if total_count <= 0:
-#                 message = "All author’s average similarity lower than 0.2"
-
-#             page = request.args.get('page', 1, type=int)
-#             offset = page * limit - limit
-#             cursor.execute("SELECT * from candidate WHERE Avg_sim > 0.2 LIMIT %s OFFSET %s", (limit, offset))
-#             conn.commit()
-#             dataa = cursor.fetchall()
-#             #######################################################################
-#             pagination = Pagination(page=page, per_page=limit, offset=offset, total=total_count, record_name='user',
-#                                     css_framework='bootstrap3')
-#             return render_template("searchpage.html", entries=entries, id=id, ts=ts,
-#                                    dataa=dataa, pagination=pagination, message=message)
-
-#     if request.method == 'GET':
-#         message = ""
-        
-#         cursor.execute("SELECT COUNT(*) FROM candidate WHERE  Avg_sim > 0.2")
-#         conn.commit()
-#         total = cursor.fetchall()
-#         total_count = total[0][0]
-#         message = ""
-#         if total_count <= 0:
-#             message = "All author’s average similarity lower than 0.2"
-        
-#         page = request.args.get('page', 1, type=int)
-#         offset = page * limit - limit
-#         #####################################################################
-#         cursor.execute("SELECT * from candidate WHERE Avg_sim > 0.2 LIMIT %s OFFSET %s ", (limit, offset))
-#         conn.commit()
-#         dataa = cursor.fetchall()
-
-#         #######################################################################
-#         pagination = Pagination(page=page, per_page=limit, offset=offset, total=total_count, record_name='user',
-#                                 css_framework='bootstrap3')
-#         return render_template("searchpage.html", entries=entries, id=id, ts=ts,
-#                                dataa=dataa, pagination=pagination, message=message)
 
 def similarity(a, b):
     return 1 - spatial.distance.cosine(a, b)
@@ -208,6 +88,7 @@ def match():
         for df in dfs:
             authors += df['Authors'].tolist()
             photos += df['Photo'].tolist()   
+        
         #Create a new dataframe author photo  
         df_author_photo = pd.DataFrame({'Authors': authors,
                    'Photos': photos})
@@ -215,10 +96,8 @@ def match():
 
         df_author_photo = df_author_photo.drop_duplicates('Authors')
         df_author_photo = df_author_photo.set_index('Authors')
-        # print(df_author_photo)
-        # print(len(df_author_photo))
+        author_photo_dict = df_author_photo.to_dict('index')
 
-        #   print(authors)
         #b. Compute the authors yearly average similarity
         df_year_similarity = {}
         for df in dfs:
@@ -256,26 +135,31 @@ def match():
             for year, df in df_year_all_publications.items():
                 if author in df:
                     author_record.append({f"total_year_all_publications_{year}":df[author]})
-            # if author in df_author_photo.index:
-            #     print(author, ": True")
-            #     author_record.append({"photo":df_author_photo[author]})
-            # else:
-            #     print(author, ": false")
-            # # else:
-            # #     author_record.append({"photo":df_author_photo[author]})
+            if author in author_photo_dict:
+                author_record.append(author_photo_dict[author])
             result_author_record = reduce(lambda a, b: {**a, **b}, author_record)
             records.append(result_author_record)
         
         #Merge a list of dicts to single dicts
         df_output = pd.DataFrame(records)
-        df_output.set_index('name', inplace=True)
+        df_output = df_output.fillna(0)
+        df_output['Similarity_Score'] = df_output['average_similarity_2018'] + df_output['average_similarity_2019'] + df_output['average_similarity_2020'] + df_output['average_similarity_2021']
+        df_output['Total_Related_Publications'] = df_output['total_related_publications_2018'] + df_output['total_related_publications_2019'] + df_output['total_related_publications_2020'] + df_output['total_related_publications_2021']
+        df_output['Total_Publications'] = df_output['total_year_all_publications_2018'] + df_output['total_year_all_publications_2019'] + df_output['total_year_all_publications_2020'] + df_output['total_year_all_publications_2021']
+        
+        #change some of the columns datatype to int
+        df_output = df_output.astype({"Total_Related_Publications": int}, errors='raise') 
+        df_output = df_output.astype({"Total_Publications": int}, errors='raise') 
+
+        
+        df_output = df_output.set_index('name')
+        df_output = df_output.sort_values(by='Similarity_Score', ascending=False)
 
         #Output dataframe to csv
         df_output.to_csv('sample.csv')
 
         #Output dataframe to flask
-        data_dict = df_output.to_dict('index')
-
+        data_dict = df_output.to_dict(orient='index', into=OrderedDict)
         return render_template("searchpage.html", output_data = data_dict)
         
 
